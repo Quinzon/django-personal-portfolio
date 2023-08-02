88ea0b87d78bd2ea41fd65399367733e1981047e
Роман Попов <popov_roman7@mail.ru>
2023-08-02 20:02:32 +0800

    Final

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.TextField()
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Resume(models.Model):
+    file = models.FileField(upload_to='portfolio/resume/')
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Project
+from .models import Resume
 
 
 def portfolio(request):
     projects = Project.objects.all()
-    return render(request, 'portfolio/portfolio.html', {'projects': projects})
+    resume = Resume.objects.first()
+    return render(request, 'portfolio/portfolio.html', {'projects': projects, 'resume': resume})
